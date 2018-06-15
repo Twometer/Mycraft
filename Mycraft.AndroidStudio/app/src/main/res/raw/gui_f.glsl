6b509fb19e62c6ab890c263a6ec0a3627ea917a5
Twometer <twometer@outlook.de>
2018-06-16 00:01:10 +0200

    Added shaders to files

---
+++ Mycraft.AndroidStudio/app/src/main/res/raw/gui_f.glsl
@@ -0,0 +1,8 @@
+#version 300 es
+precision mediump float;
+in vec4 fragmentColor;
+out vec4 out_Colour;
+
+void main(void) {
+    out_Colour = fragmentColor;
+}
---
+++ Mycraft.AndroidStudio/app/src/main/res/raw/gui_v.glsl
@@ -0,0 +1,8 @@
+#version 300 es
+layout(location = 0) in vec2 position;
+layout(location = 1) in vec4 vertexColor;
+out vec4 fragmentColor;
+void main(void) {
+    gl_Position = vec4(position, 0.0, 1.0);
+    fragmentColor = vertexColor;
+}
